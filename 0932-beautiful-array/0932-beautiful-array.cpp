edd147447e399e5b505cf3079b15e688a3bd099e
Mahak Bansal <mahakbansal86@gmail.com>
2026-07-29 17:32:26 +0530

    Time: 3 ms (37.61%), Space: 10.2 MB (30.67%) - LeetHub

---
+++ 0932-beautiful-array/0932-beautiful-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> beautifulArray(int n) {
+
+        vector<int> result;
+        result.push_back(1);
+
+        while (result.size() < n) {
+
+            vector<int> temp;
+
+            for (int x : result) {
+                if (2 * x - 1 <= n) {
+                    temp.push_back(2 * x - 1);
+                }
+            }
+
+            for (int x : result) {
+                if (2 * x <= n) {
+                    temp.push_back(2 * x);
+                }
+            }
+
+            result = temp;
+        }
+
+        return result;
+    }
+};
